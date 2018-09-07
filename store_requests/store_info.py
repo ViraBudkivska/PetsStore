9b1d432bb1b9de86fd6d5ca9ae7c74c8d395ddf1
ViraBudkivska <budkivska@gmail.com>
2018-09-07 16:40:36 +0300

    1

---
+++ store_requests/store_info.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+
+class Store:
+    url = 'https://petstore.swagger.io/v2'
+    payload = {
+  "id": 1,
+  "petId": 1,
+  "quantity": 1,
+  "shipDate": "",
+  "status": "placed",
+  "complete": False
+               }
+
+
+    def create_store(self):
+        response = requests.post(self.url + '/store/order', json=self.payload)
+        data = json.loads(response.text)
+        return data, response
+
+
+    def get_store_inventory(self):
+        response = requests.get(self.url + '/store/inventory', json=self.payload)
+        data = json.loads(response.text)
+        return data, response
+
+
+    def get_store_by_id(self):
+        response = requests.get(self.url + '/store/order/' + str(self.payload['id']), json=self.payload)
+        data = json.loads(response.text)
+        return data, response
+
+
+    def delete_pet_by_id(self):
+        response = requests.delete(self.url + '/store/order/' + str(self.payload['id']), json=self.payload)
+        data = json.loads(response.text)
+        return data, response
+
+
