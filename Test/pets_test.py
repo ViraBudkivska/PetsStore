26345e9f15a10e341d4c4ddc5b2a86643da4b5bb
ViraBudkivska <budkivska@gmail.com>
2018-09-11 18:37:02 +0300

    Revert "1"
    This reverts commit 5b762b26f54017e8e9c47e23a4842f4c46f9a12d.
    
    # Conflicts:
    #	Test/pets_test.py
    #	pets/pet_info.py

--- Test/pets_test.py
+++ Test/pets_test.py
@@ -6,7 +6,7 @@ from pets.pet import pet_object
 def test_create_pet():
     response_from_function = pet_object.create_pet()
     # assert response_from_function[1].status_code == 200
-    assert response_from_function[0]['id'] == pet_object.create_pet.payload['id']
+    assert response_from_function[0]['id'] == response_from_function[1][id]
 
 
 def test_get_pet_by_id():
@@ -35,7 +35,7 @@ def test_delete_pet():
 
 
 def test_invalid_pet_by_id():
-    response_from_function = pet_manager.get_invalid_pet_by_id()
+    response_from_function = pet_object.get_invalid_pet_by_id()
     assert response_from_function[1].status_code == 404
 
 
