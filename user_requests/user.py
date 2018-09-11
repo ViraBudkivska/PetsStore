1cc8f59c65e7d2987d19c2ddd3fa14889b14214d
ViraBudkivska <budkivska@gmail.com>
2018-09-11 17:09:38 +0300

    Revert "1"
    This reverts commit 5b762b26f54017e8e9c47e23a4842f4c46f9a12d.
    
    # Conflicts:
    #	Test/pets_test.py
    #	pets/pet_info.py

--- user_requests/user.py
+++ user_requests/user.py
@@ -1,19 +1,27 @@
 import requests
-import json
+
+
+# class User:
+#
+#     payload = {
+#   "id": 1,
+#   "username": "Nini",
+#   "firstName": "Lidiya",
+#   "lastName": "Kozak",
+#   "email": "nini@gfnnf.com",
+#   "password": "123456",
+#   "phone": "0967667575",
+#   "userStatus": 1
+#          }
 
 
 class User:
-    url = 'https://petstore.swagger.io/v2'
-    payload = {
-  "id": 1,
-  "username": "Nini",
-  "firstName": "Lidiya",
-  "lastName": "Kozak",
-  "email": "nini@gfnnf.com",
-  "password": "123456",
-  "phone": "0967667575",
-  "userStatus": 1
-         }
+
+    payload: dict = {}
+
+    def __init__(self, **kwargs):
+        self.payload = dict(**kwargs)
+        self.url = 'https://petstore.swagger.io/v2'
 
     def create_user(self):
         response = requests.post(self.url + '/user', json=self.payload)
@@ -22,8 +30,6 @@ class User:
     def get_user_username(self):
         response = requests.get(self.url + '/user/' + str(self.payload['username']), json=self.payload)
         return response
-
-
 
     def update_user_username(self):
         self.payload['username'] = "Nono"
@@ -34,3 +40,10 @@ class User:
         response = requests.delete(self.url + '/user/' + str(self.payload['username']))
         return response
 
+
+user_object = User(id=1, username="Ninih", firstName="Vira", lastName="Budda", email="email@gmail.com", password="1234567", phone="55455545", userStatus=0)
+
+user_object.create_user()
+user_object.get_user_username()
+user_object.update_user_username()
+user_object.delete_user_username()
